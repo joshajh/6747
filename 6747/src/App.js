2a0d6a5776b18054c440881b94bdbe84282572b2
joshajh <65796160+joshajh@users.noreply.github.com>
2024-02-03 12:02:15 +0000

    initial commit

---
+++ 6747/src/App.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import WavesurferPlayer from '@wavesurfer/react'
+
+const App = () => {
+  const [wavesurfer, setWavesurfer] = useState(null)
+  const [isPlaying, setIsPlaying] = useState(false)
+
+  const onReady = (ws) => {
+    setWavesurfer(ws)
+    setIsPlaying(false)
+  }
+
+  const onPlayPause = () => {
+    wavesurfer && wavesurfer.playPause()
+  }
+
+  return (
+    <>
+      <WavesurferPlayer
+        height={100}
+        waveColor="violet"
+        url="https://pssblwrlds.xyz:8010/radio.mp3"
+        onReady={onReady}
+        onPlay={() => setIsPlaying(true)}
+        onPause={() => setIsPlaying(false)}
+      />
+
+      <button onClick={onPlayPause}>
+        {isPlaying ? 'Pause' : 'Play'}
+      </button>
+    </>
+  )
+}
+
+export default App
